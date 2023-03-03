b06a021fc00522fc3c4ba48b3c026179cb4a9a3a
PreLeyZero <Paul.Zschoppe@gmx.de>
2023-03-03 02:04:59 +0100

    fixed that logistic containers translate well

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,3 +6,8 @@ ei_containers_lib = require("lib/lib")
 
 -- swap steel chest with 1x1 container
 ei_containers_lib.swap_item_in_recipe("steel-chest", "ei_1x1-container")
+ei_containers_lib.swap_item_in_recipe("logistic-chest-passive-provider", "ei_1x1-container_red")
+ei_containers_lib.swap_item_in_recipe("logistic-chest-active-provider", "ei_1x1-container_pink")
+ei_containers_lib.swap_item_in_recipe("logistic-chest-storage", "ei_1x1-container_yellow")
+ei_containers_lib.swap_item_in_recipe("logistic-chest-requester", "ei_1x1-container_blue")
+ei_containers_lib.swap_item_in_recipe("logistic-chest-buffer", "ei_1x1-container_green")
